16441861fac778f06e1cc9faee24b7ea6c514e35
梁绍飞 <583515646@qq.com>
2021-05-01 12:33:56 +0800

    !1 init push * init commit

---
+++ src/Nodes/ClassDeclarationNode.ts
@@ -0,0 +1,39 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import { DeclarationNode } from "./DeclarationNode";
+
+export class ClassDeclarationNode extends DeclarationNode
+{
+	// #region Constructors (1)
+
+	constructor(className: string, isExport: boolean, isAbstract: boolean, parent: DeclarationNode | null, children: DeclarationNode[], command: vscode.Command, start: vscode.Position, end: vscode.Position)
+	{
+		super();
+
+		this.name = className;
+		this.label = className;
+
+		this.start = start;
+		this.end = end;
+
+		this.parent = parent;
+		this.children = children;
+		this.command = command;
+
+
+		this.iconPath = {
+			light: path.join(this.imageDir, 'Class_light.svg'),
+			dark: path.join(this.imageDir, 'Class_dark.svg')
+		};
+
+		if (!isExport)
+		{
+			console.log(this.imageDir);
+			
+
+			this.label += " ";
+		}
+	}
+
+	// #endregion
+}
---
+++ src/Nodes/DeclarationNode.ts
@@ -0,0 +1,55 @@
+import * as path from "path";
+import * as vscode from "vscode";
+
+export abstract class DeclarationNode extends vscode.TreeItem
+{
+	// #region Properties (8)
+
+	protected readonly imageDir = path.join(__filename, "..", "..", "..", "resources", "images", "tree_icons");
+
+	public children: DeclarationNode[] = [];
+	public end: vscode.Position = new vscode.Position(0, 0);
+	public name: string | null = null;
+	public parent: DeclarationNode | null = null;
+	public start: vscode.Position = new vscode.Position(0, 0);
+
+	// #endregion
+
+	// #region Constructors (1)
+
+	constructor()
+	{
+		super("", vscode.TreeItemCollapsibleState.Expanded);
+	}
+
+	// #endregion
+
+	// #region Public Accessors (1)
+
+	public get tooltip(): string
+	{
+		return this.label!;
+	}
+
+	// #endregion
+
+	// #region Protected Methods (1)
+
+	protected convertHexToString(input: string)
+	{
+		var inputHex = input.match(/[\s\S]{2}/g) || [];
+		var output = '';
+
+		for (var i = 0, j = inputHex.length; i < j; i++)
+		{
+			output += '%' + ('0' + inputHex[i]).slice(-2);
+		}
+
+		output = decodeURIComponent(output);
+		
+		
+		return output;
+	}
+
+	// #endregion
+}
